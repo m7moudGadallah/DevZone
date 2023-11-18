dd82b490814244ae8ccda57a51ff9ddf5dab8277
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-11-18 08:39:17 +0200

    Add database configruation in `config/index.js`
    - Add method to select database based on environment.
    - Add config object
    - Initailze database by createing and instatance from it

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,37 @@
 const dotenv = require('dotenv');
+const { Database } = require('./database');
 
 const configEnv = `${__dirname}/../../.env`;
 dotenv.config({ path: configEnv }); // Load environment variables
 
-module.exports = {};
+// Database configuration
+
+/**
+ * Choose the database URL based on the environment
+ * @returns {string} Database URL
+ */
+const selectDatabase = () => {
+  if (process.env.NODE_ENV === 'production') {
+    return process.env.DATABASE_URL_PROD;
+  }
+  if (process.env.NODE_ENV === 'testing') {
+    return process.env.DATABASE_URL_TEST;
+  }
+  return process.env.DATABASE_URL_DEV;
+};
+
+/**
+ * Database configuration
+ * @type {{URL: string, NAME: string}}}
+ * @property {string} URL - Database URL
+ * @property {string} NAME - Database name
+ */
+const DATABASE_CONFIG = Object.freeze({
+  URL: selectDatabase(),
+  NAME: selectDatabase().split('/').pop(),
+});
+
+// Initialize database
+Database.getInstance(DATABASE_CONFIG.URL);
+
+module.exports = { DATABASE_CONFIG, Database };
