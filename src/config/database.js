8727b06d1279d1250305954dec82e1a0bfa8a267
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-11-18 08:36:16 +0200

    Add 'config/database.js` module
    - This module has Database Class which will manage prismaClient using
      singleton design pattern.

---
+++ src/config/database.js
@@ -0,0 +1,52 @@
+const { PrismaClient } = require('@prisma/client');
+
+/**
+ * Database class to manage Prisma Client using Singleton design pattern.
+ */
+class Database {
+  static #dbClientInstance = null;
+
+  /**
+   * Get an instance of Prisma Client
+   * @param {string | undefined} databaseUrl Database URL
+   * @returns {import('@prisma/client').PrismaClient} Prisma Client instance
+   */
+  static getInstance(databaseUrl) {
+    if (!Database.#dbClientInstance) {
+      try {
+        Database.#dbClientInstance = new PrismaClient(
+          databaseUrl && {
+            datasources: {
+              db: {
+                url: databaseUrl,
+              },
+            },
+          }
+        );
+      } catch (error) {
+        console.error('Error while initializing Prisma Client: ', error);
+        throw error;
+      }
+    }
+
+    return Database.#dbClientInstance;
+  }
+
+  /**
+   * disconnect Prisma Client from database
+   * @async
+   * @returns {Promise<void>}
+   */
+  static async disconnect() {
+    try {
+      if (Database.#dbClientInstance) {
+        await Database.#dbClientInstance.$disconnect();
+      }
+    } catch (error) {
+      console.error('Error while disconnecting Prisma Client: ', error);
+      throw error;
+    }
+  }
+}
+
+module.exports = { Database };
