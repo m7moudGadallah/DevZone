3c104d896aa68b4f16c11f2b076113e5469f6abf
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-11-18 07:09:12 +0200

    Add `server.js` module
    - This module contains Server class that used to handle httpServer
      intailization and closing httpServer.

---
+++ src/server.js
@@ -0,0 +1,51 @@
+const http = require('http');
+require('colors');
+require('./config');
+
+/**
+ * HTTP Server class
+ */
+class Server {
+  static #httpServerInstance = null;
+
+  /**
+   * Initialize the HTTP server
+   * @param {import('express').Application} app Express app
+   * @returns {http.Server} HTTP server
+   */
+  static initialize(app) {
+    if (Server.#httpServerInstance) return Server.#httpServerInstance;
+
+    const { PORT = 3000, NODE_ENV: MODE } = process.env;
+
+    // Create the HTTP server
+    Server.#httpServerInstance = http.createServer(app);
+
+    // Listen to the specified port
+    Server.#httpServerInstance.listen(PORT, () => {
+      console.log(
+        'App is running in '.brightMagenta.underline.bold.italic +
+          `${MODE}`.brightYellow.underline.bold.italic +
+          ' mode on port '.brightMagenta.underline.bold.italic +
+          `${PORT}`.brightYellow.underline.bold.italic +
+          ' 🚀...'.brightMagenta.underline.bold.italic
+      );
+    });
+
+    return Server.#httpServerInstance;
+  }
+
+  /**
+   * Close the HTTP server
+   * @returns {void}
+   */
+  static close() {
+    if (Server.#httpServerInstance) {
+      Server.#httpServerInstance.close(() => {
+        console.log('HTTP server closed'.red);
+      });
+    }
+  }
+}
+
+module.exports = { Server };
